4366ce20048764e30bfae413b50b2f1892c748d0
Darth-Carrotpie <carrotpie@gmail.com>
2017-12-29 12:55:37 +0200

    fixed start/stop button funcionallity, added handler functions for led.on/off

--- HS.py
+++ HS.py
@@ -11,10 +11,10 @@ class MainClass(object):
     def __init__(self):
         self.root = tk.Tk()
         myFont=tkinter.font.Font(family='Helvetica', size = 12, weight = "bold")
-        led1 = LED(21)
-        led1.on()   ##this is tu turn led off needed cause my MOSFETs invert signal
-        led2 = LED(20)
-        led2.on()
+        self.led1 = LED(21)
+        self.led1.on()   ##this is tu turn led off needed cause my MOSFETs invert signal
+        self.led2 = LED(20)
+        self.led2.on()
         
 ##        self.root.grid_columnconfigure(3, weight=3)
         self.var = tk.StringVar()
@@ -24,14 +24,14 @@ class MainClass(object):
         value = tk.Label(self.root, textvariable=self.var, width=30)
         #humidity = tk.Label(self.root, textvariable=self.varH, width=20)
 
-        #label.grid(row=0, column=0)
         value.grid(row=0, column=0)
-        #humidity.grid(row=0, column=2) #, sticky="w"
 
+        self.fingerExtended = tk.BooleanVar()
         #Valve Manual Manipulation Buttons Testing:
-        ledButton=tk.Button(self.root, text = 'Switch Blue', font=myFont, command=lambda: self.ledToggle(led1, led2), bg='bisque2', height=1, width=24)
+        ledButton=tk.Button(self.root, text = 'Pull', font=myFont, command=lambda:self.setState(False), bg='bisque2', height=1, width=24)
         ledButton.grid(row=1, sticky=tk.NSEW)
-        ledButton=tk.Button(self.root, text = 'Switch Green', font=myFont, command=lambda: self.ledToggle(led2, led1), bg='bisque2', height=1, width=24)
+        ledButton=tk.Button(self.root, text = 'Push', font=myFont, command=lambda:self.setState(True), bg='bisque2', height=1, width=24)
+   #        ledButton=tk.Button(self.root, text = 'Push', font=myFont, command=lambda: self.setState(led2, led1), bg='bisque2', height=1, width=24)
         ledButton.grid(row=2, sticky=tk.NSEW)
 
         #Valve Auto
@@ -40,15 +40,11 @@ class MainClass(object):
 
         self.started = tk.BooleanVar()
 
-        startButton=tk.Button(self.root, text = 'Start Hitting', font=myFont, command=lambda: self.startButton(), bg='bisque2', height=1, width=12)
-
-        if self.started:
-            startButton.grid(row=4, sticky=tk.E)
-            #StartButton.text = 'Stop Hitting'
-        else:
-            startButton.grid(row=4, sticky=tk.W)
-            #startButton.text = 'Start Hitting'
         
+        
+        self.startButton=tk.Button(self.root, text = 'Start Hitting', font=myFont, command=self.startButton, bg='bisque2', height=1, width=12)
+        self.startButton.grid(row=4, sticky=tk.W)
+
         #App specific:
         exitButton=tk.Button(self.root, text='Exit', font=myFont, command=self.exitProgram, bg='cyan', height=1, width=6)
         exitButton.grid(row=5, sticky=tk.E)
@@ -65,13 +61,27 @@ class MainClass(object):
         
     def startButton(self):
         self.started = not self.started
-        
-    def ledToggle(self, led, otherLed):
-        if led.is_lit:
-            led.off()
-            otherLed.on()
+        #print(self.started)
+        if self.started:
+            self.startButton.grid(row=4, sticky=tk.E)
+            self.startButton["text"] = 'Stop Hitting'
         else:
-            led.on()
+            self.startButton.grid(row=4, sticky=tk.W)
+            self.startButton["text"] = 'Start Hitting'
+        
+    def setState(self, extend):
+        if extend:
+            self.ledToggle(self.led2, self.led1)
+            self.fingerExtended = True;
+        else:
+            self.ledToggle(self.led1, self.led2)
+            self.fingerExtended = False;            
+            
+    def ledToggle(self, led, otherLed):
+        otherLed.on()
+        led.off()
+        self.root.after(10)
+        led.on()
             
     def start(self):
         # start the sensors
@@ -92,6 +102,7 @@ class MainClass(object):
             #self.varH.set(message["humidity"])
         self.root.after(500, self.poll_queue)
     def exitProgram(self):
+        self.sensor.stop()
         self.root.quit()
 
 if __name__ == "__main__":
--- Sensors.py
+++ Sensors.py
@@ -4,7 +4,7 @@ import Adafruit_DHT
 import time
 class THSensor(threading.Thread):
     def __init__(self, queue):
-        threading.Thread.__init__(self)
+        threading.Thread.__init__(self)        
         self.queue = queue
         self.stop_requested = False
 
@@ -12,15 +12,14 @@ class THSensor(threading.Thread):
         self.stop_requested = True
 
     def run(self):
-        for i in range(10):
+        exit_flag = threading.Event()
+        while not exit_flag.wait(timeout=1):
             print('read')
             if self.stop_requested:
                 break
             ##            value = random.randint(10, 100)
             humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
-            temperature = float(temperature)
-            humidity = float(humidity)
             if temperature is not None and humidity is not None:
-                print('values are not None')
+                temperature = float(temperature)
+                humidity = float(humidity)
                 self.queue.put({"humidity": humidity, "temperature": temperature})
-            time.sleep(1)
