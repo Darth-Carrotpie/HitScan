e22e02fb7e9f8f0f68a58b4ec61bf817b7bb76e8
Darth-Carrotpie <carrotpie@gmail.com>
2018-01-11 14:21:00 +0200

    added music button and ico

--- HS.py
+++ HS.py
@@ -5,6 +5,8 @@ import sensors as sns
 import finger as fng
 import voltmeter
 import logger
+import pygame
+from PIL import Image, ImageTk
 class MainClass(object):
 
     def __init__(self):
@@ -22,18 +24,23 @@ class MainClass(object):
         self.pressureVar = tk.StringVar()
         pressureLabel = tk.Label(self.root, textvariable=self.pressureVar, font=textFont)
         pressureLabel.grid(row=1, column=1)
-
+        
+        #battery info:
+        self.batteryFrame = tk.Frame(self.root, highlightbackground="yellow", highlightcolor="yellow", highlightthickness=2, width=100, height=100, bd=0)
+        self.batteryFrame.grid(row=0, column=2, sticky=tk.NSEW)
+        
         self.voltageInputVar = tk.StringVar()
-        voltageInputLabel = tk.Label(self.root, textvariable=self.voltageInputVar, font=textFont)
-        voltageInputLabel.grid(row=4, column=1)
+        voltageInputLabel = tk.Label(self.batteryFrame, textvariable=self.voltageInputVar, font=textFont)
+        voltageInputLabel.grid(row=0, column=0, sticky=tk.NSEW)
 
         self.voltageOutputVar = tk.StringVar()
-        voltageOutputLabel = tk.Label(self.root, textvariable=self.voltageOutputVar, font=textFont)
-        voltageOutputLabel.grid(row=5, column=1)
+        voltageOutputLabel = tk.Label(self.batteryFrame, textvariable=self.voltageOutputVar, font=textFont)
+        voltageOutputLabel.grid(row=1, column=0, sticky=tk.NSEW)
         
+        #altitude, sea level:
         self.extraDataVar = tk.StringVar()
         extraLabel = tk.Label(self.root, textvariable=self.extraDataVar, font=textFont)
-        extraLabel.grid(row=6, column=1,sticky=tk.W)
+        extraLabel.grid(row=5, column=1,sticky=tk.NSEW)
         
         self.fingerExtended = tk.BooleanVar()
 
@@ -44,50 +51,70 @@ class MainClass(object):
         readSensorButton.grid(row=0, column=0, rowspan=2, sticky=tk.NSEW)
         
         #Valve Manual Manipulation Buttons:
-        ledButton=tk.Button(self.root, text = 'Pull', font=myFont, command=lambda:self.finger.setState(self, False), bg='DarkSeaGreen1', height=1, width=15)
-        ledButton.grid(row=2, column=0, sticky=tk.NSEW)
-        ledButton=tk.Button(self.root, text = 'Push', font=myFont, command=lambda:self.finger.setState(self, True), bg='DarkSeaGreen1', height=1, width=15)
-        ledButton.grid(row=3, column=0, sticky=tk.NSEW)
+        ledButton=tk.Button(self.root, text = 'Pull', font=myFont, command=lambda:self.finger.setState(self, False), bg='DarkSeaGreen1', height=1, width=8)
+        ledButton.grid(row=2, column=2, sticky=tk.NSEW)
+        ledButton=tk.Button(self.root, text = 'Push', font=myFont, command=lambda:self.finger.setState(self, True), bg='DarkSeaGreen1', height=1, width=8)
+        ledButton.grid(row=3, column=2, sticky=tk.NSEW)
         
         singleCycle=tk.Button(self.root, text = 'Single Hit', font=myFont, command=lambda:self.finger.cycle(self), bg='DarkSeaGreen1', height=2, width=15)
         singleCycle.grid(row=2, column=1, rowspan=2, sticky=tk.NSEW)
 
         #Valve Auto
-        autoAreaLabel = tk.Label(self.root, text = 'Set Automatic Hit Cycle:')
-        autoAreaLabel.grid(row=4, column=0)
+        #autoAreaLabel = tk.Label(self.root, text = 'Set Automatic Hit Cycle:')
+        #autoAreaLabel.grid(row=1, column=0)
 
         self.bpm = tk.DoubleVar()
         self.bpm.set(120)
         self.delayPortion = tk.DoubleVar()
         self.delayPortion.set(0.5)
-        
-        self.startButton=tk.Button(self.root, text = 'Start Hitting', font=myFont, command=self.startButton, bg='OliveDrab1', height=1, width=8)
-        self.startButton.grid(row=5, sticky=tk.W)
 
         #Valve Auto Input
         self.inputFrame = tk.Frame(self.root, highlightbackground="green", highlightcolor="green", highlightthickness=2, width=100, height=100, bd=0)
-        self.inputFrame.grid(row=6, column=0, sticky=tk.NSEW)
+        self.inputFrame.grid(row=2, column=0, rowspan=2, sticky=tk.NSEW)
+        
+        self.startButton=tk.Button(self.inputFrame, text = 'Start Hitting', font=myFont, command=self.startButton, bg='OliveDrab1', height=1, width=8)
+        self.startButton.grid(row=0, columnspan=2, sticky=tk.NSEW)
         
         vcmd = (self.root.register(self.validate),'%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
         
-        autoAreaLabel = tk.Label(self.inputFrame, text = 'BPM:')
-        autoAreaLabel.grid(row=0, column=0)
+        autoAreaLabel = tk.Label(self.inputFrame, text = 'BPM:', width=5)
+        autoAreaLabel.grid(row=1, column=0)
         #autoAreaLabel.place(relx=.0, rely=.5, anchor="w")
-        self.bpmInput = tk.Entry(self.inputFrame, textvariable=self.bpm, validate = 'key', validatecommand = vcmd, width=7)
-        self.bpmInput.grid(row=0, column=1, sticky=tk.NSEW)
+        self.bpmInput = tk.Entry(self.inputFrame, textvariable=self.bpm, validate = 'key', validatecommand = vcmd, width=9)
+        self.bpmInput.grid(row=1, column=1, sticky=tk.NSEW)
         
-        autoAreaLabel = tk.Label(self.inputFrame, text = 'U/D:')
-        autoAreaLabel.grid(row=0, column=3)        
-        self.speedInput = tk.Entry(self.inputFrame, textvariable=self.delayPortion, validate = 'key', validatecommand = vcmd, width=7)
-        self.speedInput.grid(row=0, column=4, sticky=tk.NSEW)
+        autoAreaLabel = tk.Label(self.inputFrame, text = 'U/D:', width=5)
+        autoAreaLabel.grid(row=1, column=2)        
+        self.speedInput = tk.Entry(self.inputFrame, textvariable=self.delayPortion, validate = 'key', validatecommand = vcmd, width=9)
+        self.speedInput.grid(row=1, column=3, sticky=tk.NSEW)
+        
+        #extras - audio, video, etc:
+        self.extrasFrame = tk.Frame(self.root, highlightbackground="RoyalBlue1", highlightcolor="RoyalBlue1", highlightthickness=2, width=100, height=100, bd=0)
+        self.extrasFrame.grid(row=4, column=0, rowspan=2, sticky=tk.NSEW)        
+        image = Image.open("/home/pi/Documents/HitScan/Resources/playstop.ico")
+        image = image.resize((64, 64), Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+        audioPath = "/home/pi/Documents/HitScan/Audio"
+        #get audio file list, take first, return path as string:
+######
+        #play button:
+        playStopImageLabel = tk.Button(self.extrasFrame, image=photo, font=myFont, command=lambda:self.playAudio(path), bg='RoyalBlue1', height=64, width=64)
+        playStopImageLabel.grid(row=0, column=0) 
+        playStopImageLabel.image = photo # keep a reference!
+        playStopImageLabel.pack()
         
         #App specific:
         exitButton=tk.Button(self.root, text='Exit', font=myFont, command=self.exitProgram, bg='CadetBlue4', height=1, width=3)
-        exitButton.grid(row=6, column=1, sticky=tk.E)
+        exitButton.grid(row=5, column=2, sticky=tk.E)
         self.logger = logger.SensorLogger(self)
 
         self.sensor = sns.THSensor(self)
         self.finger = fng.Control(self)
+        
+    def playAudio(self, path):
+        pygame.mixer.music.load(fullpath)
+	pygame.mixer.music.play()
+	
     def validate(self, action, index, value_if_allowed,
                        prior_value, text, validation_type, trigger_type, widget_name):
         if text in '0123456789.-+':
@@ -108,7 +135,7 @@ class MainClass(object):
         self.started.set (not self.started.get())
         #print(self.started.get())
         if self.started.get():
-            self.startButton.grid(row=5, sticky=tk.E)
+            self.startButton.grid(column=2)
             self.startButton["text"] = 'Stop Hitting'
             self.startButton["bg"] = 'tomato'
             if not self.bpm.get():
@@ -116,7 +143,7 @@ class MainClass(object):
             if not self.delayPortion.get():
                 self.delayPortion.set(0.5)
         else:
-            self.startButton.grid(row=5, sticky=tk.W)
+            self.startButton.grid(column=0)
             self.startButton["text"] = 'Start Hitting'
             self.startButton["bg"] = 'OliveDrab1'
 
